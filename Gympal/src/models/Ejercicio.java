47a72ad847c1eddc19297bef8e90ecec01e12fa6
Alejandro081998 <ale.salgado08@hotmail.com>
2022-11-15 21:29:56 -0300

    Ejercicios
    Agregue 50 ejercicios - 10 por cada musculo, para empezar a trabajar.

--- Gympal/src/models/Ejercicio.java
+++ Gympal/src/models/Ejercicio.java
@@ -5,49 +5,60 @@ import models.enums.GrupoMuscular;
 
 public class Ejercicio {
 
-    private Integer series;
-    private Integer repeticiones;
-    private Float pesoAsignado;
-    private Integer nivelAerobico;
-    private ExigenciaMuscular exigenciaMuscular;
-    private GrupoMuscular grupoMuscular;
-    private String videoUrl;
+	private Integer series;
+	private Integer repeticiones;
+	private String descripcion;
+	private Float pesoAsignado;
+	private Integer nivelAerobico;
+	private ExigenciaMuscular exigenciaMuscular;
+	private GrupoMuscular grupoMuscular;
+	private String videoUrl;
 
-    public Ejercicio(Integer series, Integer repeticiones, Float pesoAsignado, Integer nivelAerobico, ExigenciaMuscular exigenciaMuscular, GrupoMuscular grupoMuscular, String videoUrl) {
-        this.series = series;
-        this.repeticiones = repeticiones;
-        this.pesoAsignado = pesoAsignado;
-        this.nivelAerobico = nivelAerobico;
-        this.exigenciaMuscular = exigenciaMuscular;
-        this.grupoMuscular = grupoMuscular;
-        this.videoUrl = videoUrl;
-    }
+	public Ejercicio(String descripcion, Integer series, Integer repeticiones, Float pesoAsignado,
+			Integer nivelAerobico, ExigenciaMuscular exigenciaMuscular, GrupoMuscular grupoMuscular, String videoUrl) {
+		this.descripcion = descripcion;
+		this.series = series;
+		this.repeticiones = repeticiones;
+		this.pesoAsignado = pesoAsignado;
+		this.nivelAerobico = nivelAerobico;
+		this.exigenciaMuscular = exigenciaMuscular;
+		this.grupoMuscular = grupoMuscular;
+		this.videoUrl = videoUrl;
+	}
 
-    public Integer getSeries() {
-        return series;
-    }
+	public Integer getSeries() {
+		return series;
+	}
 
-    public Integer getRepeticiones() {
-        return repeticiones;
-    }
+	public Integer getRepeticiones() {
+		return repeticiones;
+	}
 
-    public Float getPesoAsignado() {
-        return pesoAsignado;
-    }
+	public Float getPesoAsignado() {
+		return pesoAsignado;
+	}
 
-    public Integer getNivelAerobico() {
-        return nivelAerobico;
-    }
+	public Integer getNivelAerobico() {
+		return nivelAerobico;
+	}
 
-    public ExigenciaMuscular getExigenciaMuscular() {
-        return exigenciaMuscular;
-    }
+	public ExigenciaMuscular getExigenciaMuscular() {
+		return exigenciaMuscular;
+	}
 
-    public GrupoMuscular getGrupoMuscular() {
-        return grupoMuscular;
-    }
+	public GrupoMuscular getGrupoMuscular() {
+		return grupoMuscular;
+	}
 
-    public String getVideoUrl() {
-        return videoUrl;
-    }
+	public String getVideoUrl() {
+		return videoUrl;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 }
